e616ec841fab3a8317a53edda22561105de4804a
Colin Adler <colin@coder.com>
2020-04-09 11:28:34 -0500

    Allow dynamic namespaces for statagg

--- statagg/src/client.rs
+++ statagg/src/client.rs
@@ -3,6 +3,7 @@ mod gatewaypb {
 }
 
 use gatewaypb::gateway_client;
+use std::env;
 use std::error::Error;
 use tonic::transport::channel::Channel;
 
@@ -10,10 +11,11 @@ pub type GatewayClient = gateway_client::GatewayClient<Channel>;
 pub use gatewaypb::{EmptyRequest, Stat, StatsResponse};
 
 pub async fn get_clients(clusters: i32) -> Result<Vec<GatewayClient>, Box<dyn Error>> {
+    let ns = env::var("NAMESPACE").unwrap_or("tatsu".to_string());
     let mut conns: Vec<GatewayClient> = Vec::with_capacity(clusters as usize);
 
     for i in 0..clusters {
-        let url = format!("http://gateway-{}.gateway.tatsu.svc.cluster.local", i);
+        let url = format!("http://gateway-{}.{}.tatsu.svc.cluster.local", i, ns);
         println!("{}", url);
         let conn = GatewayClient::connect(url).await?;
         conns.push(conn);

0ab9e10109e1324f7ba4a6e4fda3957b9a1a08d0
benson <benson@tatsu.works>
2020-10-21 12:14:08 +0800

    Decrease timeout duration to more aggressively resume

--- internal/gatewayws/read.go
+++ internal/gatewayws/read.go
@@ -10,17 +10,19 @@ import (
 	"github.com/tatsuworks/czlib"
 )
 
+const connectionTimeout = 10
+
 // readMessage populates buf on *Session with the next message.
 func (s *Session) readMessage() error {
 	start := time.Now()
 	defer func() {
 		took := time.Since(start)
-		if took > 20*time.Second {
-			s.log.Error(s.ctx, "took more than 20s to get reader", slog.F("took", time.Since(start).String()))
+		if took > connectionTimeout*time.Second {
+			s.log.Error(s.ctx, "took too long to get reader", slog.F("took", time.Since(start).String()))
 		}
 	}()
 
-	ctx, cancel := context.WithTimeout(s.ctx, 20*time.Second)
+	ctx, cancel := context.WithTimeout(s.ctx, connectionTimeout*time.Second)
 	defer cancel()
 
 	_, r, err := s.wsConn.Reader(ctx)

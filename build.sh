7d2f70ca7bd4064ab04a3390af4a4ea7cbabadf1
Teh Hao Rui Marcus <marcusteh1238@gmail.com>
2021-05-06 06:49:36 +0000

    add ovh uri to build script

--- build.sh
+++ build.sh
@@ -10,11 +10,14 @@ if [[ $VERSION == *-dirty ]]; then
   VERSION+="-$(head -c 5 < /dev/urandom | base32)"
 fi
 
-gateway_uri="gcr.io/tatsu-production/gateway:$VERSION"
+# uri="gcr.io/tatsu-production/gateway:$VERSION"
+uri="6222o0k9.gra7.container-registry.ovh.net/tatsu/gateway:$VERSION"
 docker build -t "$gateway_uri" -f Dockerfile.gateway .
 docker push "$gateway_uri"
 
-state_uri="gcr.io/tatsu-production/state:$VERSION"
+# uri="gcr.io/tatsu-production/state:$VERSION"
+uri="6222o0k9.gra7.container-registry.ovh.net/tatsu/state:$VERSION"
+
 docker build -t "$state_uri" -f Dockerfile.state .
 docker push "$state_uri"
 

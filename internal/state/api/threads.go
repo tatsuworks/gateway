45aaea4e7cf7fdaf2cb62f15f9ba4345e3da52e3
ngoccuongbka94@gmail.com <ngoccuongbka94@gmail.com>
2021-05-14 20:15:27 +0700

    fix redeclare error

--- internal/state/api/threads.go
+++ internal/state/api/threads.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"encoding/binary"
-	"io"
 	"net/http"
 	"strconv"
 
@@ -57,83 +55,4 @@ func (s *Server) getGuildThreads(w http.ResponseWriter, r *http.Request, p httpr
 	}
 
 	return s.writeTerms(w, cs)
-}
-
-// Erlang external term tags.
-const (
-	ettStart = byte(131)
-	ettList  = byte(108)
-	ettNil   = byte(106)
-)
-
-func (s *Server) writeTerms(w io.Writer, raws [][]byte) error {
-	if s.enc == "etf" {
-		if err := writeSliceHeader(w, len(raws)); err != nil {
-			return err
-		}
-	} else if s.enc == "json" {
-		w.Write([]byte("["))
-	}
-
-	first := true
-	writeComma := func() {
-		if first {
-			first = false
-			return
-		}
-
-		w.Write([]byte(","))
-	}
-
-	for _, e := range raws {
-		writeComma()
-		_, err := w.Write(e)
-		if err != nil {
-			return xerrors.Errorf("write term: %w", err)
-		}
-	}
-
-	if s.enc == "etf" {
-		_, err := w.Write([]byte{ettNil})
-		if err != nil {
-			return xerrors.Errorf("failed to write ending nil: %w", err)
-		}
-	} else if s.enc == "json" {
-		w.Write([]byte("]"))
-	}
-
-	return nil
-}
-
-func (s *Server) writeTerm(w io.Writer, raw []byte) error {
-	if s.enc == "etf" {
-		if err := writeETFHeader(w); err != nil {
-			return err
-		}
-	}
-
-	_, err := w.Write(raw)
-	return err
-}
-
-func writeSliceHeader(w io.Writer, len int) error {
-	var h [6]byte
-	h[0], h[1] = ettStart, ettList
-	binary.BigEndian.PutUint32(h[2:], uint32(len))
-
-	_, err := w.Write(h[:])
-	if err != nil {
-		return xerrors.Errorf("write slice header: %w", err)
-	}
-
-	return nil
-}
-
-func writeETFHeader(w io.Writer) error {
-	_, err := w.Write([]byte{131})
-	if err != nil {
-		return xerrors.Errorf("write etf starting byte: %w", err)
-	}
-
-	return nil
 }

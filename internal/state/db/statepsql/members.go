d596cc06630231189a5b80799b0bc1840e64319e
Benson Chua <u0707198@gmail.com>
2022-03-17 16:57:11 +0800

    Perform JSON conversions

--- internal/state/db/statepsql/members.go
+++ internal/state/db/statepsql/members.go
@@ -3,6 +3,7 @@ package statepsql
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"strconv"
 	"strings"
 	"unsafe"
@@ -144,15 +145,24 @@ WHERE
 }
 
 func (db *db) GetGuildMembersWithRole(ctx context.Context, guildID, roleID int64) ([][]byte, error) {
-	const q = `select user_id from members where guild_id = $1 and  data->'roles' ? $2`
+	const q = `select user_id::TEXT from members where guild_id = $1 and  data->'roles' ? $2`
 
-	var ms []RawJSON
+	var ms []string
 	err := db.sql.SelectContext(ctx, &ms, q, guildID, roleID)
 	if err != nil {
 		return nil, xerrors.Errorf("exec select: %w", err)
 	}
-
-	return *(*[][]byte)(unsafe.Pointer(&ms)), nil
+	res := make([][]byte, len(ms))
+	for i, id := range ms {
+		user := map[string]string{"id": id}
+		jsonUser, err := json.Marshal(user)
+		if err != nil {
+			return nil, xerrors.Errorf("json marshal: %w", err)
+		}
+		res[i] = jsonUser
+		return nil, xerrors.Errorf("exec select: %w", err)
+	}
+	return res, nil
 }
 
 func (db *db) DeleteGuildMembers(ctx context.Context, guildID int64) error {

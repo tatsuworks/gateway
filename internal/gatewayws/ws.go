da7d1aebc828529386e3c6de904242f1ae85e210
benson <benson@tatsu.works>
2025-08-21 21:21:24 +0800

    Increase wait time for guild members population to stabilize during event handling

--- internal/gatewayws/ws.go
+++ internal/gatewayws/ws.go
@@ -27,6 +27,8 @@ import (
 
 const (
 	IdentifyMutexRootName = "/gateway/identify/"
+	IdentifyWaitTime      = 10 * time.Second
+	IdentifyStabilizeTime = 60 * time.Second
 )
 
 type Session struct {
@@ -405,7 +407,10 @@ func (s *Session) handleInternalEvent(ev *discord.Event) (bool, error) {
 		s.ready = time.Now()
 
 		go func() {
-			time.Sleep(7 * time.Second)
+			time.Sleep(IdentifyWaitTime)
+			if s.hasGuildMembersIntent { // allow for more time to process database when getting guild members population
+				time.Sleep(IdentifyStabilizeTime)
+			}
 			err = s.releaseIdentifyLock()
 			if err != nil {
 				s.log.Error(s.ctx, "release identify lock after ready", slog.Error(err))
